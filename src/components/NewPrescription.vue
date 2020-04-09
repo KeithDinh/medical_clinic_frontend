d567ad27772d24e022eec193c54557db85c83acf
thieny1991 <thieny1991@yahoo.com>
2020-04-08 21:25:28 -0500

    update new prescription form with auto filled data

--- src/components/NewPrescription.vue
+++ src/components/NewPrescription.vue
@@ -1,38 +1,58 @@
 <template>
   <div class="container">
   <form @submit.prevent="handleSubmit">
-  <div class="row">
-    <div class="col-30">
-      <label >Medication Name</label>
+    <div class="row">
+      <div class="col-30">
+        <label >By</label>
+      </div>
+      <div class="col-70">
+        <input type="text" id="doctor_name" name="doctorName"  v-model="doctor.firstName +' '+doctor.lastName" disabled>
+      </div>
     </div>
-    <div class="col-70">
-      <select type="text" id="medication_name" name="medication name">
-        <option v-for="meds in medications.medicationNames[0]" v-bind:value="meds.medication_name" >{{meds.medication_name}}</option>
-      </select>
-    </div>
-  </div>
 
     <div class="row">
-    <div class="col-30">
-      <label >Medication Form</label>
+      <div class="col-30">
+        <label >DoctorID</label>
+      </div>
+      <div class="col-70">
+        <input type="text" id="doctor_id" name="doctorId"  v-model="doctor.doctorId" disabled>
+      </div>
     </div>
-    <div class="col-70">
-      <select type="text" id="medication_forms" name="medication form">
-        <option v-for="meds in medications.medicationForms[0]" v-bind:value="meds.dose_form_name" >{{meds.dose_form_name}}</option>
-      </select>
+
+
+    <div class="row">
+      <div class="col-30">
+        <label >Medication Name</label>
+      </div>
+      <div class="col-70">
+        <select type="text" id="medication_name" name="medication name">
+          <option v-for="meds in medications.medicationNames[0]" v-bind:value="meds.medication_name" >{{meds.medication_name}}</option>
+        </select>
+      </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="col-30">
-      <label for="subject">Subject</label>
+
+      <div class="row">
+      <div class="col-30">
+        <label >Medication Form</label>
+      </div>
+      <div class="col-70">
+        <select type="text" id="medication_forms" name="medication form">
+          <option v-for="meds in medications.medicationForms[0]" v-bind:value="meds.dose_form_name" >{{meds.dose_form_name}}</option>
+        </select>
+      </div>
     </div>
-    <div class="col-70">
-      <textarea id="subject" name="subject" placeholder="Dosage and instruction" style="height:100px"></textarea>
+
+    <div class="row">
+      <div class="col-30">
+        <label for="subject">Subject</label>
+      </div>
+      <div class="col-70">
+        <textarea id="subject" name="subject" placeholder="Dosage and instruction" style="height:100px"></textarea>
+      </div>
     </div>
-  </div>
-  <div class="row">
-    <input type="submit" value="Submit">
-  </div>
+    <div class="row">
+      <input type="submit" value="Submit">
+    </div>
   </form>
 </div>
 </template>
@@ -42,24 +62,47 @@ import { mapState, mapActions } from 'vuex';
 import DoctorProfile from "./DoctorProfile";
 export default {
   name: 'NewPrescription',
-  components: {},
+  data () {
+    return {
+      apptId :'',
+      doctorId : '',
+      patientId :'',
+      medicationId :'',
+      doseForm :'',
+      dosage :'',
+      datePrescribed :''
+      ,
+      submitted: false
+    }
+  },
+    created() {
+    this.loadDoctorMedications(),
+      this.loadDoctorProfile
+  },
   watch: {
     medications: function () {
-      this.loadDoctorMedications()
+      this.loadDoctorMedications(),
+         this.loadDoctorProfile()
     }
   },
   computed: {
     ...mapState('doctor', {
-      medications: state => state.doctorMedsList
-    })
-  },
-  created() {
-    this.loadDoctorMedications()
-  },
+      medications: state => state.doctorMedsList,
+      doctor: state => state.doctorProfile
+    }),
+    ...mapState('profile',{
+      patient: state => state.userProfile
+    }),
+    },
+
   methods: {
     ...mapActions('doctor', [
       'loadDoctorMedications',
+      'loadDoctorProfile',
     ]),
+     addNewPescription (e) {
+      this.submitted = true
+    }
   }
 }
 </script>
