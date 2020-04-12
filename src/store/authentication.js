0b145e1132f2277147a794004fd86bb4a4f73fd8
Mayoorshardha <60448086+Mayoorshardha@users.noreply.github.com>
2020-04-11 20:35:08 -0500

    Update authentication.js

--- src/store/authentication.js
+++ src/store/authentication.js
@@ -6,10 +6,63 @@ const initialState = localUser
   ? { status: { loggedIn: true }, localUser: localUser }
   : { status: {}, localUser: null }
 
+//validate doctor registration
+const validateDoctorRegistration = (firstName, middleInit, lastName,phone, specialistId,gender,email,passwordOne, passwordTwo, race,dob, street, city, state, zipcode) => {
+  let errors = []
+  const re = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  
+  if (!re.test(email)) {
+    errors.push(new Error('Valid email required.'))
+  }
+  if (!passwordOne) {
+    errors.push(new Error('Password required.'))
+  }
+  if (!passwordTwo) {
+    errors.push(new Error('Must confirm password.'))
+  }
+  if (passwordOne !== passwordTwo) {
+    errors.push(new Error('Passwords must match.'))
+  }
+  if (!firstName) {
+    errors.push(new Error('First name required.'))
+  }
+  if (!lastName) {
+    errors.push(new Error('Last name required.'))
+  }
+  if (!street) {
+    errors.push(new Error('Street address required.'))
+  }
+  if (!city) {
+    errors.push(new Error('city required.'))
+  }
+  if (!state) {
+    errors.push(new Error('State required.'))
+  }
+  if (!zipcode) {
+    errors.push(new Error('Zipcode required.'))
+  }
+  if (!phone) {
+    errors.push(new Error('Phone number required.'))
+  }
+  if (!dob) {
+    errors.push(new Error('Date of birth required.'))
+  }
+  if (!gender) {
+    errors.push(new Error('Gender required.'))
+  }
+  if (!specialistId) {
+    errors.push(new Error('Marital status required.'))
+  }
+  if (!race) {
+    errors.push(new Error('Race required.'))
+  }
+  return errors
+}
 // Validate the Registration Form
 const validateRegistration = (email, passwordOne, passwordTwo, firstName, middleInit, lastName, street, city, state, zipcode, phone, dob, gender, marital, race) => {
   let errors = []
   const re = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
   if (!re.test(email)) {
     errors.push(new Error('Valid email required.'))
   }
@@ -158,7 +211,7 @@ export const authentication = {
     },
     register ({dispatch, commit}, { email, passwordOne, passwordTwo, firstName, middleInit, lastName, street, city, state, zipcode, phone, dob, gender, marital, race }) {
       commit('registerRequest', {email, passwordOne, passwordTwo, firstName, middleInit, lastName, street, city, state, zipcode, phone, dob, gender, marital, race})
-      let errors = validateRegistration(email, passwordOne, passwordTwo, firstName, middleInit, lastName, street, city, state, zipcode, phone, dob, gender, marital, race)
+      let errors = validateRegistration(firstName, middleInit, lastName,phone, specialistId,gender,email,passwordOne, passwordTwo, race,dob, street, city, state, zipcode)
       if (errors.length === 0) {
         userService.register(email, passwordOne, firstName, middleInit, lastName, street, city, state, zipcode, phone, dob, gender, marital, race)
           .then(
@@ -181,6 +234,34 @@ export const authentication = {
         commit('registerFailure', errors)
         dispatch('alert/error', errors, { root: true })
       }
-    }
+    },
+    registerDoctor ({dispatch, commit}, { firstName, middleInit, lastName,phone, specialistId,gender,email,password, race,dob, street, city, state, zipcode, image} ) {
+      commit('registerRequest', { firstName, middleInit, lastName,phone, specialistId,gender,email,password, race,dob, street, city, state, zipcode, image})
+      alert("hihi", firstName)
+      let errors = validateDoctorRegistration({ firstName, middleInit, lastName,phone, specialistId,gender,email,password, race,dob, street, city, state, zipcode})
+      if (errors.length === 0) {
+        userService.registerDoctor({ firstName, middleInit, lastName,phone, specialistId,gender,email,passwordOne, passwordTwo, race,dob, street, city, state, zipcode})
+          .then(
+            localUser => {
+              alert("I am in register sucess")
+              commit('registerSuccess', localUser)
+              doctorService.getDoctor(localUser.doctor_id).then(
+                response => {
+                  router.push('/admin')
+                },
+                error => {
+                  alert('registrationFailure')
+                })
+            },
+            error => {
+              commit('registerFailure', error)
+              dispatch('alert/error', error, { root: true })
+            }
+          )
+      } else {
+        commit('registerFailure', errors)
+        dispatch('alert/error', errors, { root: true })
+      }
+    },
   }
 }
