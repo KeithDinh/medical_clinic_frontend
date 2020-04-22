a56bc3a5e662f94dba8310fa449992c9a33652ce
thieny1991 <thieny1991@yahoo.com>
2020-04-21 21:19:49 -0500

    update

--- src/components/MedicalRecords.vue
+++ src/components/MedicalRecords.vue
@@ -41,10 +41,10 @@
 </template>
 <script>
 
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex';
 import NewRecord from "./NewRecord";
 import NewPrescription from "./NewPrescription";
-import MedicalRecordUpdate from "./MedicalRecordUpdate"
+import MedicalRecordUpdate from "./MedicalRecordUpdate";
 export default {
 
   name: 'MedicalRecords',
@@ -52,58 +52,62 @@ export default {
   created () {
     this.loadMedicalRecords()
   },
-  component:{
-    NewRecord,
-   data: function () {
-    return {
-      isOpen: false,
-      isHidden: false,
-      singleRecord: {}
-    }
-  },
-  computed: {
-    ...mapState('medicalRecords', {
-      records: state => state.recordsList
-    }),
-     userStatus () {return this.$store.state.authentication}
-  },
-  methods: {
-    ...mapActions('medicalRecords', [
-      'loadMedicalRecords'
-    ]),
-     addClicked: function () {
-      if (this.isOpen) {
-        this.isOpen = false
-      } else {
-        this.isOpen = true
+    data: function () {
+      return {
+        isOpen: false,
+        isHidden: false,
+        singleRecord: {}
       }
     },
-    popUpModal(obj){this.isHidden = true;this.singleRx = obj;},
-    disableModal(){this.isHidden = false;},
-  },
-  filters: {
-    frontEndTimeFormat(str) {
-      var dateobj = new Date(str);
-      var hours = ("0" + dateobj.getUTCHours()).slice(-2)
-      var minutes = ("0" + dateobj.getUTCMinutes()).slice(-2)
-      return hours + ":" + minutes;
+    computed: {
+      ...mapState('medicalRecords', {
+        records: state => state.recordsList
+      }),
+      userStatus() {
+        return this.$store.state.authentication
+      }
     },
-    frontEndDateFormat(str) {
-      const dateobj = new Date(str);
-      const month = ('0' + (dateobj.getMonth() + 1)).slice(-2);
-      const date = ('0' + dateobj.getDate()).slice(-2);
-      const year = dateobj.getFullYear();
-      return month + "/" + date + "/" + year;
+    methods: {
+      ...mapActions('medicalRecords', [
+        'loadMedicalRecords'
+      ]),
+      addClicked: function () {
+        if (this.isOpen) {
+          this.isOpen = false
+        } else {
+          this.isOpen = true
+        }
+      },
+      popUpModal(obj) {
+        this.isHidden = true;
+        this.singleRx = obj;
+      },
+      disableModal() {
+        this.isHidden = false;
+      },
     },
-    booleanFormat(str){
-      if(str=="0")
-        return "No"
-      else
-        return "Yes"
-    }
-  },
-
-}
+    filters: {
+      frontEndTimeFormat(str) {
+        var dateobj = new Date(str);
+        var hours = ("0" + dateobj.getUTCHours()).slice(-2)
+        var minutes = ("0" + dateobj.getUTCMinutes()).slice(-2)
+        return hours + ":" + minutes;
+      },
+      frontEndDateFormat(str) {
+        const dateobj = new Date(str);
+        const month = ('0' + (dateobj.getMonth() + 1)).slice(-2);
+        const date = ('0' + dateobj.getDate()).slice(-2);
+        const year = dateobj.getFullYear();
+        return month + "/" + date + "/" + year;
+      },
+      booleanFormat(str) {
+        if (str == "0")
+          return "No"
+        else
+          return "Yes"
+      }
+    },
+  }
 
 
 </script>
